f3133addc0e6e2f6bf2a968d71e7c0c5226a06a8
Samyak Bhuta <samyak.bhuta@gmail.com>
2013-10-15 05:19:33 +0530

    Partially confirms to specification. Many thanks to 'optimist' module.
    - Milliseconds, seconds, minutes, hours values are taken as input.
    - As per convention, long as well as short flags are allowed. e.g -s or --seconds
    - Cummulative values for total duration possible. '-k500 -s2' will yield
      2500 milliseconds as total duration.
    - '--help' is supported. See comments in code for more.
    - Many TODOs added as well. See comments in code for more.
    - Updated package.json for dependencies, version bump.

--- node/countdown.js
+++ node/countdown.js
@@ -1,7 +1,65 @@
 #!/usr/bin/env node
+var optimist = require('optimist');
 
+
+// TODO : Add finer usage text. Modularise it.
+var usage = 'Starts countdown for specified duration.\nUsage: $0 [-k num] [-s num] [-m num] [-h num]';
+
+var argv = optimist
+	.usage(usage)
+	.wrap(80)
+	.boolean('help')
+	.describe('help','Prints this help information. All other options are ignored.')
+	.options({'k' : {
+		alias : 'milliseconds',
+		default : 0,
+		describe : 'duration in milliseconds'
+	},
+	's' : {
+		alias : 'seconds',
+		default : 0,
+		describe : 'duration in seconds'
+	},
+	'm' : {
+		alias : 'minutes',
+		default : 0,
+		describe : 'duration in minutes'
+	},
+	'h' : {
+		alias : 'hours',
+		default : 0,
+		describe : 'duration in hours'
+	}})
+    .argv;
+
+// Check if command has been invoked with ```--help``.
+// If so,
+// - Should only print the help information.
+// - Ignore other options.
+// - Exit with success.
+if (argv.help) {
+	optimist.showHelp();
+	process.exit(code=0);
+}
+
+// Total milliseconds to countdown, set to 0 as initial value
+var duration = 0;
+
+// Adds to duration what all options are given during command invocation.
+duration += argv.k;
+duration += argv.s*1000;
+duration += argv.m*1000*60;
+duration += argv.h*1000*60*60;
+
+// TODO : Obligatory information to use. May be we should remove it or make it better.
+console.log('Countdown set for %d milliseconds', duration);
+
+
+// The function that is called after the countdown ends.
+// By default it prints ```'Time is now!'``` to console.
+// TODO : What action is performed should be configurable.
 var announce = function() {
     console.log('Time is now!');
 }
 
-setTimeout(announce,process.argv[2]);
+setTimeout(announce, duration);
